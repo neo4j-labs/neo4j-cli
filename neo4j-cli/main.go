ed59b2c8516b4fe1fb2e5b23d89bb88b55d1b7c7
Oskar Hane <oh@oskarhane.com>
2026-05-05 14:41:20 +0200

    remove-debug-logs-in-output-task-001: strip debug fmt.Printf from neo4j-cli/main.go

--- neo4j-cli/main.go
+++ neo4j-cli/main.go
@@ -28,19 +28,9 @@ func main() {
 	cmd.SetOut(os.Stdout)
 	cmd.SetErr(os.Stderr)
 
-	origHelp := cmd.HelpFunc()
-	cmd.SetHelpFunc(func(c *cobra.Command, args []string) {
-		fmt.Printf("[neo4j-cli] help displayed: %s\n", c.CommandPath()) // TODO: remove this log in favour of real metrics on help displayed
-		origHelp(c, args)
-	})
-
 	cobra.EnableTraverseRunHooks = true
 
-	// cobra prints the error itself; we only add the hook for errors that bypassed
-	// both RunE and HelpFunc (e.g. unknown top-level command via legacyArgs in Find).
 	if err := cmd.Execute(); err != nil {
-		fmt.Printf("[neo4j-cli] invalid command with args %s: %v\n", os.Args[1:], err) // TODO: remove this log in favour of real metrics in case of invalid command
 		os.Exit(1)
 	}
-	fmt.Printf("[neo4j-cli] command executed successfully with args %s\n", os.Args[1:]) // TODO: remove this log in favour of real metrics on successful command execution
 }
